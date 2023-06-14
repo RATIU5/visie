a2d5be2ca6f99ee9a81762c16363ded9fcd12dc7
RATIU5 <jcmemmott20@outlook.com>
2023-06-14 16:30:36 -0600

    core: trying rust

--- src/core/index.ts
+++
@@ -1,8 +0,0 @@
-const runApp = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  console.log("Hello World");
-  ctx.strokeRect(0, 0, 100, 100);
-};
-
-export default runApp;
